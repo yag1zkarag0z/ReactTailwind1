70508d194db38a05e4ae2b3b823f04ca2619ddf6
yagiz <yagocevat123@gmail.com>
2026-06-30 15:50:53 +0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React from "react";
-import Navbar from "./components/Navbar/Navbar";
-import Hero from "./components/Hero/Hero";
+import Navbar from "./components/navbar/navbar";
+import Hero from "./components/hero/Hero";
 import Products from "./components/Products/Products";
 import AOS from "aos";
 import "aos/dist/aos.css";
